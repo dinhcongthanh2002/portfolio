b6254cbe2e47817b512f0a72d6cf1bd6cea5f442
gnt.thanhdc <dinhcongthanha10@gmail.com>
2025-03-05 17:24:47 +0700

    feat(layout): Replace default footer with AppFooter component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -3,17 +3,16 @@
 import { Layout } from "antd";
 import AppHeader from "./Header";
 import "antd/dist/reset.css";
+import AppFooter from "./Footer";
 
-const { Content, Footer } = Layout;
+const { Content } = Layout;
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Layout className="min-h-screen flex justify-center items-center">
       <AppHeader />
       <Content className="w-full max-w-6xl p-6">{children}</Content>
-      <Footer className="text-center">
-        © {new Date().getFullYear()} Dinh Cong Thanh. All rights reserved.
-      </Footer>
+      <AppFooter />
     </Layout>
   );
 };
